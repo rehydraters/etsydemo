f71e15b150f51e3647b8b90e508478af45070a32
rehydraters <rehydraters@gmail.com>
2016-02-05 23:03:51 -0600

    initial commit

---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionController::TestCase
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+
+  test "should get content" do
+    get :content
+    assert_response :success
+  end
+
+end
